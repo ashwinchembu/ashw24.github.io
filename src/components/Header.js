c92f2b8fd371c4b526423d88bd19894c5f628606
ashw24 <66876291+ashw24@users.noreply.github.com>
2023-02-07 21:41:29 -0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from '../assets/images/logo.svg';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo} width= "250%" height= "300%" alt="AC" /></span>
+        <span className="logo"><img src={logo} width= "50rem" height= "60rem" alt="AC" /></span>
         <h1>Ashwin</h1>
         <p>Software Developer<br/>
         </p>
