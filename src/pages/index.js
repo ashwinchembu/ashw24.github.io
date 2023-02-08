417d9befc3b55f8d13e2855f6f8e715fb6202ab4
ashw24 <66876291+ashw24@users.noreply.github.com>
2023-02-07 19:31:27 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ class Index extends React.Component {
                 </p>
                 <ul className="actions">
                   <li>
-                    <Link to="/generic" className="button">
+                    <Link to="https://www.linkedin.com/in/ashwin-chembu-a5292b205/" className="button">
                       Learn More
                     </Link>
                   </li>
