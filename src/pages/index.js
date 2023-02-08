f1e3bdad3239ba07e1d9ee072d87808f1263265b
ashw24 <66876291+ashw24@users.noreply.github.com>
2023-02-07 20:10:50 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,14 +66,14 @@ class Index extends React.Component {
             </header>
             <ul className="features">
               <li>
-                <span className="fa-diagram-project"></span>
+                <span className="icon fa-diagram-project"></span>
                 <h3>Six Degrees of Kevin Bacon</h3>
                 <p>
                   In this project, I developed a C++ program that utilizes a graph data structure and breadth-first search to find the shortest path between any two actors in the IMDb database. 
                 </p>
               </li>
               <li>
-                <span className="fa-puzzle"></span>
+                <span className="icon fa-puzzle"></span>
                 <h3>Sudoku/SAT Solver</h3>
                 <p>
                   I wrote a Python program that uses the boolean satisfiability (SAT) problem to solve Sudoku puzzles. Given a partially filled-in grid, the goal is to fill in the remaining cells such that every row, column, and 3x3 subgrid contains the digits 1 through 9 without repetitions. 
