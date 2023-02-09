74da75038c3f8d8857912cacd15ca26680d54132
ashw24 <66876291+ashw24@users.noreply.github.com>
2023-02-08 18:00:12 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,32 +3,57 @@ const Footer = props => (
     <section>
       <h2>Little About Me</h2>
       <p>
-        I am a highly motivated and competitive college student with a passion for technology and computer science. I have a solid foundation in these areas and possess certifications in data science and cloud computing, along with hands-on experience. I am a skilled time manager, a team player, and a dedicated individual who can listen carefully, complete tasks efficiently, and make meaningful contributions to a team. 
+        I am a highly motivated and competitive college student with a passion for technology and computer science. I have a solid foundation in these areas and possess certifications in data science and cloud computing, along with hands-on experience. I am a skilled time manager, a team player, and a dedicated individual who can listen carefully, complete tasks efficiently, and make meaningful contributions to a team.
       </p>
     </section>
     <section>
-      <h2>Contact Info</h2>
+      <h2>Etiam feugiat</h2>
       <dl className="alt">
+        <dt>Address</dt>
+        <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
         <dt>Phone</dt>
-        <dd>(341) 356-1722</dd>
+        <dd>(000) 000-0000 x 0000</dd>
         <dt>Email</dt>
         <dd>
-          <a href="ashchembu@gmail.com">ashchembu@gmail.com</a>
+          <a href="https://codebushi.com">information@untitled.tld</a>
         </dd>
+      </dl>
+      <ul className="icons">
         <li>
           <a
-            href="https://github.com/ashw24"
+            href="https://twitter.com/huntaroSan"
+            className="icon fa-twitter alt"
+          >
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://codebushi.com" className="icon fa-facebook alt">
+            <span className="label">Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://codebushi.com" className="icon fa-instagram alt">
+            <span className="label">Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/codebushi/gatsby-starter-stellar"
             className="icon fa-github alt"
           >
             <span className="label">GitHub</span>
           </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/ashwin-chembu-a5292b205/" className="icon fa-linkedin alt">
-            <span className="label">LinkedIn</span>
+          <a href="https://codebushi.com" className="icon fa-dribbble alt">
+            <span className="label">Dribbble</span>
           </a>
         </li>
       </ul>
     </section>
+    <p className="copyright">
+      &copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.
+    </p>
   </footer>
 )
