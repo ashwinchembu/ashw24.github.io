7cd51a32f62fe5b8eace80397fd18e059c7b4d21
ashw24 <66876291+ashw24@users.noreply.github.com>
2023-02-10 14:59:09 -0800

    changed logo and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo} alt="" /></span>
+        <span className="logo"><img src={logo} width = {500} height = {600} alt="Logo" /></span>
         <h1>Ashwin</h1>
-        <p>Aspriing Software Developer</p>
+        <p>Aspring Software Developer</p>
     </header>
 )
 
