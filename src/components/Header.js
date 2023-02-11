be95c6de7f214fc815402333eddb2b2206c77870
ashw24 <66876291+ashw24@users.noreply.github.com>
2023-02-11 10:57:51 -0800

    changed image size

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo} width = {500} height = {500} alt="Logo" /></span>
+        <span className="logo"><img src={logo} width = "200rem" height = "200rem" alt="Logo" /></span>
         <h1>Ashwin</h1>
         <p>Aspring Software Developer</p>
     </header>
